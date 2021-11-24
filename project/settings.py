6879e2e6ba34671793a22221d4f67e52bedf215a
unknown <j.thiagobf1992@gmail.com>
2021-11-24 11:32:15 -0300

    primeiro envio

--- project/settings.py
+++ project/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sistema',
-        'PORT': '3306',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
